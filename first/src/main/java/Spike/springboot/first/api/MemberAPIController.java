565ebb800aee143004069f58bafa452a9b6eaa2c
yoonho <qufdbsgh431@gmail.com>
2023-10-26 14:56:43 +0900

    REST API 서비스 계층 추가 복습

--- first/src/main/java/Spike/springboot/first/api/MemberAPIController.java
+++ first/src/main/java/Spike/springboot/first/api/MemberAPIController.java
@@ -1,5 +1,6 @@
 package Spike.springboot.first.api;
 
+import Spike.springboot.first.api.service.MemberService;
 import Spike.springboot.first.dto.MemberForm;
 import Spike.springboot.first.entity.Member;
 import Spike.springboot.first.repository.MemberRepository;
@@ -19,13 +20,8 @@ public class MemberAPIController {
 
     // 생성자 자동 주입
     private final MemberRepository memberRepository;
+    private final MemberService memberservice;
 
-    // 더미 데이터 생성
-    @PostConstruct
-    public void init() {
-        memberRepository.save(new Member(1L, "qwer@gmail.com", "1234"));
-        memberRepository.save(new Member(2L, "qwer22@gmail.com", "1234444"));
-    }
 
     @GetMapping("/hello/member")
     public String hello() {
@@ -35,50 +31,41 @@ public class MemberAPIController {
     // 전체 조회
     @GetMapping("/api/members")
     public List<Member> members() {
-        return memberRepository.findAll();
+        return memberservice.findAll();
     }
 
     // id로 조회
     @GetMapping("/api/members/{id}")
     public Member findById(@PathVariable Long id) {
-        return memberRepository.findById(id).orElse(null);
+        return memberservice.findById(id);
     }
 
     // 생성
     @PostMapping("/api/members/create")
     public Member create(@RequestBody MemberForm memberForm) {
-        Member member = memberForm.toEntity();
-        Member savedMember = memberRepository.save(member);
-        return savedMember;
+        return memberservice.save(memberForm);
     }
 
     // 수정
     @PatchMapping("/api/members/{id}")
     public ResponseEntity<Object> updateById(@PathVariable Long id, @RequestBody MemberForm form) {
-        log.info("요청 id={}, 엔티티={}", id, form.toEntity());
-        Member member = form.toEntity();
-        if (id != member.getId() || id == null) {
-            log.info("잘못된 요청입니다. id={}, 엔티티={}", id, form.toEntity());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 수정 로직
-        Member target = memberRepository.findById(id).orElse(null);
-        target.update(member);
-        Member save = memberRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(save);
+
+        Member updatedMember = memberservice.update(id, form);
+
+        if (updatedMember == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);}
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedMember);
+
     }
 
     //삭제
     @DeleteMapping("/api/members/{id}")
-    public ResponseEntity<Object> deleteById(@PathVariable Long id, @RequestBody MemberForm form) {
-        Member member = memberRepository.findById(id).orElse(null);
-
+    public ResponseEntity<Object> deleteById(@PathVariable Long id) {
+        Member member = memberservice.delete(id);
         if (member == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 삭제 요청입니다.");
         }
-        memberRepository.delete(member);
         return ResponseEntity.status(HttpStatus.OK).build(); //build == body(null)
-
-
     }
 }
---
+++ first/src/main/java/Spike/springboot/first/api/service/MemberService.java
@@ -0,0 +1,70 @@
+package Spike.springboot.first.api.service;
+
+
+import Spike.springboot.first.dto.MemberForm;
+import Spike.springboot.first.entity.Member;
+import Spike.springboot.first.repository.MemberRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MemberService {
+
+    @Autowired
+    private final MemberRepository memberRepository;
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member(1L, "aaa@naver.com", "1234"));
+        memberRepository.save(new Member(2L, "bbb@naver.com", "3434"));
+    }
+
+    public Member save(MemberForm form) {
+        Member member = form.toEntity();
+        return memberRepository.save(member);
+    }
+
+    public Member findById(Long id) {
+        return memberRepository.findById(id).orElse(null);
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Member update(@PathVariable Long id, @RequestBody MemberForm form) {
+        log.info("요청 id={}, 엔티티={}", id, form.toEntity());
+        Member member = form.toEntity();
+        if (id != member.getId() || id == null) {
+            log.info("잘못된 요청입니다. id={}, 엔티티={}", id, form.toEntity());
+            return null;
+        }
+        // 수정 로직
+        Member target = memberRepository.findById(id).orElse(null);
+        target.update(member);
+        Member save = memberRepository.save(target);
+        return save;
+    }
+
+    public Member delete(@PathVariable Long id) {
+        Member member = memberRepository.findById(id).orElse(null);
+
+        if (member == null) {
+            return null;
+        }
+        memberRepository.delete(member);
+        return member;
+
+    }
+}
